fdd741888071bf9ba47481888a993a713cc9d551
Sheefhuss <sheefahussain029@gmail.com>
2026-04-19 15:56:58 +0530

    Update UserProfile component with initialData prop

--- src/components/Userprofile.js
+++ src/components/Userprofile.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'; // *** REMOVED useEffect ***
 
-const BACKEND_URL = 'https://innovative-project-health-tracker-backend.onrender.com'; 
+const BACKEND_URL = 'https://innovative-project-health-tracker-backend-qa58.onrender.com'; 
 
 // Helper to generate User ID (Must match the logic in App.js)
 const generateUserId = (userName) => {
